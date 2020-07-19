07ad72f0f87a77ca7f9e9c68961f8729c8f0a34a
Anton <a.trubianov@zakaz.ua>
2020-07-19 14:36:02 +0300

    create proj

---
+++ Lago/Lago/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lago
+//
+//  Created by Anton on 7/19/20.
+//  Copyright © 2020 LUX. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
